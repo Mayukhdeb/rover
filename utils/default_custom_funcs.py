ec6f502aed11a05e791ebe6b5a36c45749fd7f3c
mayukhdeb <mayukhmainak2000@gmail.com>
2021-05-17 11:30:11 +0530

    utils: default_custom_funcs: add center arg

--- utils/default_custom_funcs.py
+++ utils/default_custom_funcs.py
@@ -1,5 +1,11 @@
-def make_custom_func(layer_number = 0, channel_number= 0): 
-    def custom_func(layer_outputs):
-        loss = layer_outputs[layer_number][channel_number].mean()
-        return -loss
+def make_custom_func(layer_number = 0, channel_number= 0, center = False):
+    if center is False: 
+        def custom_func(layer_outputs):
+            loss = layer_outputs[layer_number][channel_number].mean()
+            return -loss
+    else:
+        def custom_func(layer_outputs):
+            output = layer_outputs[layer_number][channel_number]
+            loss = output[output.shape[-2]//2, output.shape[-1]//2]
+            return -loss
     return custom_func
