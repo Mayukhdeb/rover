f0eac3246238b226e7f88fe84ef862b39050bf44
mayukhdeb <mayukhmainak2000@gmail.com>
2021-05-18 10:35:45 +0530

    add setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="rover",
+    version="0.0.0",
+    author="Mayukh Deb", 
+    author_email="mayukhmainak2000@gmail.com", 
+    description= "Reverse engineer your CNNs, in styl",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mayukhdeb/rover",
+    packages=setuptools.find_packages(),
+    install_requires= required,
+    python_requires='>=3.6',   
+    include_package_data=True,
+    keywords=[
+        "PyTorch",
+        "machine learning",
+        "neural networks",
+        "convolutional neural networks",
+        "feature visualization",
+        "optimization",
+        ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]  
+)
